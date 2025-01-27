4175f3dd61369d75e1c368e977c174d30505f701
JDRM1 <jdrms056@gmail.com>
2025-01-27 12:30:26 -0500

    >notifications module >banking module >general style fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { ClientsComponent } from './pages/clients/clients.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { InvoiceComponent } from './pages/invoice/invoice.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { BankingComponent } from './pages/banking/banking.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
 
 export const routes: Routes = [
   {
@@ -39,5 +41,13 @@ export const routes: Routes = [
   {
     path: 'expenses',
     loadComponent: () => ExpensesComponent,
+  },
+  {
+    path: 'banking',
+    loadComponent: () => BankingComponent,
+  },
+  {
+    path: 'notifications',
+    loadComponent: () => NotificationsComponent,
   }
 ];
