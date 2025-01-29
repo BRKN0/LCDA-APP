f7332f9daf32e9980a09711981ca62e6b884ff6b
JDRM1 <jdrms056@gmail.com>
2025-01-28 23:54:19 -0500

    fixed styles.scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: [
+    "./src/**/*.{html,ts,scss}",
+    "./node_modules/@angular/material/**/*.js", // Ensure Material is included
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
